332f3aa12c03d3add2b250bbcbe4d043c0cebd2f
Alexey Alter-Pesotskiy <a.alterpesotskiy@mail.ru>
2023-10-11 12:14:49 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+
+import Foundation
+import PackageDescription
+
+let package = Package(
+    name: "StreamChatSwiftUI",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v14), .macOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "StreamChatSwiftUI",
+            targets: ["StreamChatSwiftUI"]
+        )
+    ],
+    targets: [
+        .binaryTarget(name: "StreamChatSwiftUI",
+                      url: "https://github.com/GetStream/stream-chat-swiftui/releases/download/4.39.0/StreamChatSwiftUI.zip",
+                      checksum: "")
+    ]
+)
---
+++ README.md
@@ -0,0 +1,5 @@
+# Stream Chat
+
+### Please refer to the main repo for more information: [stream-chat-swiftui](https://github.com/GetStream/stream-chat-swiftui/)
+
+This repository just serves as the hosting for our Package.swift pointing to precompiled XCFrameworks.
