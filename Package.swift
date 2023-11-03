e1ede5c147ae170389f65f4acfa39fb21428c6db
martinmitrevski <martinmitrevski.oh@gmail.com>
2023-11-03 17:10:09 +0100

    Bump 4.41.0

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "StreamChatSwiftUI",
-                      url: "https://github.com/GetStream/stream-chat-swiftui/releases/download/4.40.0/StreamChatSwiftUI.zip",
-                      checksum: "38ccfb9519cd7bffa9d62484f98ab8d5208f26a5b9d4d74bcf02ad643eccb8e3")
+                      url: "https://github.com/GetStream/stream-chat-swiftui/releases/download/4.41.0/StreamChatSwiftUI.zip",
+                      checksum: "c709efe30fb4598a53c0ec2bca53c5dbda6906c3796e8d7545f1bb8d180bd63f")
     ]
 )
